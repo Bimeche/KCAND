c4ce01e8fb47986aad85436415892b984fd13d47
JoannaSassano <joannasassano@hotmail.fr>
2017-02-23 19:05:12 -0500

    beginning

---
+++ Assets/scripts/BodyBehaviour.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyBehaviour : MonoBehaviour {
+
+
+    GameObject MainCamera;
+    new Camera camera;
+    bool zoom_organ = false;
+    float x_change;
+    float y_change;
+    float z_change;
+
+    // Use this for initialization
+    void Start () {
+        MainCamera = GameObject.Find("Main Camera");
+        camera = MainCamera.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (camera.GetComponent<CameraBehaviour>().zoom_position && !zoom_organ)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var ray = camera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, 100))
+                {
+                    if (hit.collider.tag == "organ1")
+                    {
+                        x_change = 0.5f;
+                        y_change = -1.2f;
+                        z_change = 3.3f;
+                        camera.transform.localPosition = new Vector3(camera.transform.localPosition.x, camera.transform.localPosition.y + y_change, camera.transform.localPosition.z + z_change);
+                    }else if(hit.collider.tag == "organ2")
+                    {
+                        x_change = 0.5f;
+                        y_change = -1.2f;
+                        z_change = 2.9f;
+                        camera.transform.localPosition = new Vector3(camera.transform.localPosition.x, camera.transform.localPosition.y + y_change, camera.transform.localPosition.z + z_change);
+                    }
+                }
+                //racemanager = GameObject.Find("Racemanager").GetComponent<RaceManager>();
+                else if (camera.GetComponent<CameraBehaviour>().zoom_position && zoom_organ)
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        ray = camera.ScreenPointToRay(Input.mousePosition);
+
+                        if (Physics.Raycast(ray, out hit, 100))
+                        {
+                            if (hit.collider.tag == "body")
+                            {
+                                x_change = -x_change;
+                                y_change = -y_change;
+                                z_change = -z_change;
+                                camera.transform.localPosition = new Vector3(camera.transform.localPosition.x, camera.transform.localPosition.y + y_change, camera.transform.localPosition.z + z_change);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/scripts/CameraBehaviour.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour {
+
+    public bool zoom_position;
+    float y_change = 1.5f;
+    float z_change = -2f;
+    GameObject MainCamera;
+    new Camera camera;
+    // Use this for initialization
+    void Start () {
+        zoom_position = false;
+        MainCamera = GameObject.Find("Main Camera");
+        camera = MainCamera.GetComponent<Camera>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 100))
+            {
+                if(hit.collider.tag == "body")
+                {
+                    y_change = -y_change;
+                    z_change = -z_change;
+                    camera.transform.localPosition = new Vector3(camera.transform.localPosition.x, camera.transform.localPosition.y+y_change, camera.transform.localPosition.z+z_change);
+                }
+            }
+        }
+	}
+}
