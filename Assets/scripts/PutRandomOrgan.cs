1547857bf317f3265890d296f2bb955d0ab11f4e
JoannaSassano <joannasassano@hotmail.fr>
2017-03-02 17:05:27 -0500

    Sheet + beginning of the random selection of organ

---
+++ Assets/scripts/PutRandomOrgan.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PutRandomOrgan : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject.Find("Organ3").SetActive(false);
+        GameObject.Find("Organ4").SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
