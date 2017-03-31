48167631277843f3c94e15023cec11112bb83664
MorganeLeBris <morganebris@gmail.com>
2017-03-31 13:35:25 -0400

    gastrite start

---
+++ Assets/Scripts/Gastrite.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gastrite : MonoBehaviour {
+
+	public GameObject glace;
+	public GameObject chocolat;
+	public GameObject pate;
+	private int actualCase;
+
+	// Use this for initialization
+	void Start () {
+		actualCase = Random.Range(0, 3);
+
+		switch (actualCase)
+		{
+                case 0:
+                    glace.GetComponent<Renderer>().enabled = true;
+                    chocolat.GetComponent<Renderer>().enabled = true;
+                    pate.GetComponent<Renderer>().enabled = false;
+                    break;
+                case 1:
+                    glace.GetComponent<Renderer>().enabled = false;
+                    chocolat.GetComponent<Renderer>().enabled = true;
+                    pate.GetComponent<Renderer>().enabled = true;
+                    break;
+                case 2:
+                    glace.GetComponent<Renderer>().enabled = false;
+                    chocolat.GetComponent<Renderer>().enabled = false;
+                    pate.GetComponent<Renderer>().enabled = true;
+                    break;
+                case 3:
+                    glace.GetComponent<Renderer>().enabled = true;
+                    chocolat.GetComponent<Renderer>().enabled = false;
+                    pate.GetComponent<Renderer>().enabled = false;
+                    break;
+                
+            }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
